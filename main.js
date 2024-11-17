d265be260c2c079f867dbac17e769ece0ae97ebd
MoatazZewin <moatazzewin@gmail.com>
2024-11-17 18:13:57 +0200

    add features to navbar

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 const ul = document.getElementById("links");
 const toggle = document.getElementsByClassName("toggle-menu")[0];
-console.log(ul);
+const children = [...ul.children];
 
 toggle.addEventListener("click", () => {
-  console.log("insdie the ");
   ul.classList.toggle("toggle-class");
+  ul.classList.toggle("show");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    if (ul.classList.contains("show")) {
+      ul.classList.remove("toggle-class");
+    }
+  } else {
+    if (ul.classList.contains("show")) {
+      ul.classList.add("toggle-class");
+    }
+  }
+});
+console.log(children);
+
+children.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    console.log("inside the click");
+    if (ul.classList.contains("toggle-class")) {
+      ul.classList.remove("show");
+      ul.classList.remove("toggle-class");
+    }
+  });
 });
